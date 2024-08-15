f56ec862907004cdd39686e4dadc45a27bf79078
Ali H <nightcap79@hotmail.com>
2024-08-15 07:19:29 +0000

    major update

---
+++ bin/model/player.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Player {
+  String? name;
+  String? answer;
+  // DateTime timeCreated;
+  int? timeOfSubmittion;
+  Player({
+    this.answer,
+    this.name,
+    this.timeOfSubmittion,
+    // required this.timeCreated,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'answer': answer,
+      'timeOfSubmittion': timeOfSubmittion,
+    };
+  }
+
+  factory Player.fromMap(Map<String, dynamic> map) {
+    return Player(
+      name: map['name'] != null ? map['name'] as String : null,
+      answer: map['answer'] != null ? map['answer'] as String : null,
+      timeOfSubmittion: map['timeOfSubmittion'] != null
+          ? map['timeOfSubmittion'] as int
+          : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Player.fromJson(String source) =>
+      Player.fromMap(json.decode(source) as Map<String, dynamic>);
+}
--- bin/server.dart
+++ bin/server.dart
@@ -1,21 +1,86 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import 'model/player.dart';
+
+Map<String, List<Player>> gameSession = {};
 // Configure routes.
 final _router = Router()
   ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/new', _newHandler)
+  ..get('/add/<id>/', _addPlayerHandler)
+  ..post('/add/<id>/submit', _answerHandler) // ?name=<name>&answer=<answer>
+  ..get('/get/<id>/', _getResultHandler);
 
 Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
+  return Response.ok(
+    '<html><h1>Hello, World!</h1></html>',
+    headers: {
+      "content-type": 'text/html'
+    }, // text/html   text/plain    application/json  text/javascript
+  );
 }
 
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
-  return Response.ok('$message\n');
+Response _newHandler(Request req) {
+  // if (req.method != "POST") return Response.badRequest(body: 'Test OK\n');
+  int id = DateTime.now().millisecondsSinceEpoch;
+
+  gameSession['$id'] = [];
+  return Response.ok(
+    'https://vigilant-capybara-5g564qvjfpg9r-8080.app.github.dev/add/$id/',
+    headers: {"content-type": 'text/plain'},
+  );
+}
+
+Future<Response> _addPlayerHandler(Request request) async {
+  final id = request.params['id'];
+  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
+
+  // gameSession[id]!.add(Player(timeCreated: DateTime.now()));
+  Future.delayed(
+    Duration(minutes: 2),
+    () => gameSession.remove(id),
+  );
+  return Response.ok(
+    await File('${Directory.current.path}/html/input.html').readAsString(),
+    headers: {"content-type": 'text/html'},
+  );
+}
+
+Future<Response> _answerHandler(Request req, String id) async {
+  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
+  final String query = await req.readAsString();
+  // var querySplit = query.split("&");
+  //querySplit[0].substring(5); //name=nightcap79&answer=hackerone
+  // querySplit[1].substring(7);
+  Map queryParams = Uri(query: query)
+      .queryParameters; // {name: nightcap79, answer: hackerone}
+
+  if (req.method != "POST") return Response.badRequest(body: 'Test OK\n');
+  var name = queryParams["name"];
+  var answer = queryParams["answer"];
+  int now = DateTime.now().millisecondsSinceEpoch;
+
+  gameSession[id]!
+      .add(Player(timeOfSubmittion: now, answer: answer, name: name));
+
+  return Response.ok(
+    'Your game id is $id  \nname=$name  answer=$answer submissionTime=$now',
+    headers: {"content-type": 'text/plain'},
+  );
+}
+
+Response _getResultHandler(Request req, String id) {
+  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
+
+  return Response.ok(
+    jsonEncode(gameSession[id]),
+    headers: {"content-type": 'application/json'},
+  );
 }
 
 void main(List<String> args) async {

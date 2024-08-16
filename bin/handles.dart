85ec81f1345e9e42e7861ca26c0a1d313dff82a2
Ali H <nightcap79@hotmail.com>
2024-08-16 11:24:12 +0000

    major update

---
+++ bin/handles.dart
@@ -0,0 +1,74 @@
+part of 'server.dart';
+
+Map<String, List<Player>> gameSession = {};
+
+var static = createStaticHandler('files',
+    defaultDocument: 'index.html', serveFilesOutsidePath: true); //
+
+Response rootHandler(Request req) {
+  return Response.ok(
+    '<html><h1>Hello, World!</h1></html>',
+    headers: {
+      "content-type": 'text/html'
+    }, // text/html   text/plain    application/json  text/javascript
+  );
+}
+
+Response newHandler(Request req) {
+  // if (req.method != "POST") return Response.badRequest(body: 'Test OK\n');
+  int id = DateTime.now().millisecondsSinceEpoch;
+
+  gameSession['$id'] = [];
+  return Response.ok(
+    'https://vigilant-capybara-5g564qvjfpg9r-8080.app.github.dev/add/$id/',
+    headers: {"content-type": 'text/plain'},
+  );
+}
+
+Future<Response> addPlayerHandler(Request request) async {
+  final id = request.params['id'];
+  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
+
+  // gameSession[id]!.add(Player(timeCreated: DateTime.now()));
+  Future.delayed(
+    Duration(minutes: 2),
+    () => gameSession.remove(id),
+  );
+
+  return Response.ok(
+    await File('${Directory.current.path}/html/index.html').readAsString(),
+    headers: {"content-type": 'text/html'},
+  );
+}
+
+Future<Response> answerHandler(Request req, String id) async {
+  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
+  final String query = await req.readAsString();
+  // var querySplit = query.split("&");
+  //querySplit[0].substring(5); //name=nightcap79&answer=hackerone
+  // querySplit[1].substring(7);
+  Map queryParams = Uri(query: query)
+      .queryParameters; // {name: nightcap79, answer: hackerone}
+
+  if (req.method != "POST") return Response.badRequest(body: 'Test OK\n');
+  var name = queryParams["name"];
+  var answer = queryParams["answer"];
+  int now = DateTime.now().millisecondsSinceEpoch;
+
+  gameSession[id]!
+      .add(Player(timeOfSubmittion: now, answer: answer, name: name));
+
+  return Response.ok(
+    'Your game id is $id  \nname=$name  answer=$answer submissionTime=$now',
+    headers: {"content-type": 'text/plain'},
+  );
+}
+
+Response getResultHandler(Request req, String id) {
+  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
+
+  return Response.ok(
+    jsonEncode(gameSession[id]),
+    headers: {"content-type": 'application/json'},
+  );
+}
--- bin/server.dart
+++ bin/server.dart
@@ -4,84 +4,20 @@ import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_static/shelf_static.dart';
 
 import 'model/player.dart';
 
-Map<String, List<Player>> gameSession = {};
+part 'handles.dart';
+
 // Configure routes.
 final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/new', _newHandler)
-  ..get('/add/<id>/', _addPlayerHandler)
-  ..post('/add/<id>/submit', _answerHandler) // ?name=<name>&answer=<answer>
-  ..get('/get/<id>/', _getResultHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok(
-    '<html><h1>Hello, World!</h1></html>',
-    headers: {
-      "content-type": 'text/html'
-    }, // text/html   text/plain    application/json  text/javascript
-  );
-}
-
-Response _newHandler(Request req) {
-  // if (req.method != "POST") return Response.badRequest(body: 'Test OK\n');
-  int id = DateTime.now().millisecondsSinceEpoch;
-
-  gameSession['$id'] = [];
-  return Response.ok(
-    'https://vigilant-capybara-5g564qvjfpg9r-8080.app.github.dev/add/$id/',
-    headers: {"content-type": 'text/plain'},
-  );
-}
-
-Future<Response> _addPlayerHandler(Request request) async {
-  final id = request.params['id'];
-  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
-
-  // gameSession[id]!.add(Player(timeCreated: DateTime.now()));
-  Future.delayed(
-    Duration(minutes: 2),
-    () => gameSession.remove(id),
-  );
-  return Response.ok(
-    await File('${Directory.current.path}/html/input.html').readAsString(),
-    headers: {"content-type": 'text/html'},
-  );
-}
-
-Future<Response> _answerHandler(Request req, String id) async {
-  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
-  final String query = await req.readAsString();
-  // var querySplit = query.split("&");
-  //querySplit[0].substring(5); //name=nightcap79&answer=hackerone
-  // querySplit[1].substring(7);
-  Map queryParams = Uri(query: query)
-      .queryParameters; // {name: nightcap79, answer: hackerone}
-
-  if (req.method != "POST") return Response.badRequest(body: 'Test OK\n');
-  var name = queryParams["name"];
-  var answer = queryParams["answer"];
-  int now = DateTime.now().millisecondsSinceEpoch;
-
-  gameSession[id]!
-      .add(Player(timeOfSubmittion: now, answer: answer, name: name));
-
-  return Response.ok(
-    'Your game id is $id  \nname=$name  answer=$answer submissionTime=$now',
-    headers: {"content-type": 'text/plain'},
-  );
-}
-
-Response _getResultHandler(Request req, String id) {
-  if (!gameSession.containsKey(id)) return Response.notFound("Game not found");
-
-  return Response.ok(
-    jsonEncode(gameSession[id]),
-    headers: {"content-type": 'application/json'},
-  );
-}
+  ..get('/', static)
+  ..get('/test', static)
+  ..get('/new', newHandler)
+  ..get('/add/<id>/', addPlayerHandler) //_addPlayerHandler
+  ..post('/add/<id>/submit', answerHandler) // ?name=<name>&answer=<answer>
+  ..get('/get/<id>/', getResultHandler);
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
@@ -90,6 +26,8 @@ void main(List<String> args) async {
   // Configure a pipeline that logs requests.
   final handler =
       Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+
+  // final handler = Cascade().add(_router.call).add(static).handler;
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
